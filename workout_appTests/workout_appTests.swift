1d3bbe4a201c57d6e7ba38b29f6d24550fb18625
DenisFeier <denis.feier@takeofflabs.com>
2021-05-06 23:25:58 +0300

    Initial Commit

---
+++ workout_appTests/workout_appTests.swift
@@ -0,0 +1,33 @@
+//
+//  workout_appTests.swift
+//  workout_appTests
+//
+//  Created by Denis Feier on 06.05.2021.
+//
+
+import XCTest
+@testable import workout_app
+
+class workout_appTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
