56ad142a91bdcf6000977daa864a52177ac7933b
DenisFeier <denis.feier@takeofflabs.com>
2021-05-08 17:22:03 +0300

    add pods and firebase

---
+++ Pods/Target Support Files/Pods-workout_app-workout_appUITests/Pods-workout_app-workout_appUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_workout_app_workout_appUITests : NSObject
+@end
+@implementation PodsDummy_Pods_workout_app_workout_appUITests
+@end
---
+++ Pods/Target Support Files/Pods-workout_appTests/Pods-workout_appTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_workout_appTests : NSObject
+@end
+@implementation PodsDummy_Pods_workout_appTests
+@end
